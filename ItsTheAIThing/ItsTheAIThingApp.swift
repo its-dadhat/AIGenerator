7f1f8d2aa235fb30fe15f1b7d8ca23a0a0fc8cee
its-dadhat <110235982+its-dadhat@users.noreply.github.com>
2023-06-02 12:02:45 +0100

    Initial Commit

---
+++ ItsTheAIThing/ItsTheAIThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsTheAIThingApp.swift
+//  ItsTheAIThing
+//
+//  Created by Chris Bristow on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ItsTheAIThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
